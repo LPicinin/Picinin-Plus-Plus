90f6370606dff5ce004f95568ac7a9a1934b4263
Picinin <luishpj@outlook.com.br>
2020-02-02 15:15:51 -0300

    initial commit

---
+++ src/pool/TelaPrincipalController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pool;
+
+import Util.CodeAreaInit;
+import com.jfoenix.controls.JFXListView;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import org.fxmisc.richtext.CodeArea;
+
+/**
+ *
+ * @author luish
+ */
+public class TelaPrincipalController implements Initializable
+{
+
+    @FXML
+    private CodeArea caCodigo;
+    @FXML
+    private JFXListView<?> lvassembly;
+        
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        CodeAreaInit.inicializa(caCodigo);
+    }    
+
+    @FXML
+    private void evtCompila(MouseEvent event)
+    {
+    }
+    
+}
