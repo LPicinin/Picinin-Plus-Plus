751ec1859368132b4762e0474e431ef52196f7da
Aluno <Aluno@LF101m18-69247>
2020-03-13 14:30:08 -0300

    chave, if, while e for

--- src/Classes/Controle/TipoAnalise.java
+++ src/Classes/Controle/TipoAnalise.java
@@ -23,15 +23,19 @@ public class TipoAnalise
     public final static int a_inicioPrograma = 5;
     public final static int ca_declaracao = 6;
     public final static int ca_naoReconhecido = 7;
+    public final static int ca_If = 8;
+    public final static int ca_else = 9;
     
     private static final TipoAnalise a_for = new TipoAnalise(Token.tFor, ca_for);
     private static final TipoAnalise a_while = new TipoAnalise(Token.tWhile, ca_while);
     private static final TipoAnalise a_declaracao = new TipoAnalise(Token.tTipo, ca_declaracao);
     //public static final TipoAnalise a_atribuicao = new TipoAnalise(Token.tIdentificador, ca_atribuicao);
     private static final TipoAnalise a_naoReconhecido = new TipoAnalise(Token.tNaoReconhecido, ca_naoReconhecido);
+    private static final TipoAnalise a_If = new TipoAnalise(Token.tIf, ca_If);
+    private static final TipoAnalise a_else = new TipoAnalise(Token.tElse, ca_else);
    
     
-    public final static List<TipoAnalise> listaAnalises = Arrays.asList(a_for, a_while, a_declaracao/*, a_atribuicao*/, a_naoReconhecido);
+    public final static List<TipoAnalise> listaAnalises = Arrays.asList(a_for, a_while, a_declaracao/*, a_atribuicao*/, a_naoReconhecido, a_If, a_else);
     private Token first;
     private int codigo;
 
--- src/Classes/Sintatico.java
+++ src/Classes/Sintatico.java
@@ -112,11 +112,10 @@ public class Sintatico extends Constantes
             {
                 String val = "";
                 String tipo = "";
-                if(i == 0 && !lt.get(0).getToken().equals(Token.tInicio_Linguagem))
+                if (i == 0 && !lt.get(0).getToken().equals(Token.tInicio_Linguagem))
                 {
-                    
-                }
-                else if (i - 1 >= 0 && lt.get(i - 1).getToken().equals(tInicio_Linguagem))
+
+                } else if (i - 1 >= 0 && lt.get(i - 1).getToken().equals(tInicio_Linguagem))
                 {
                     tipo = lt.get(i - 1).getToken().getIdToken().replace("t", "");
                 } else
@@ -179,9 +178,9 @@ public class Sintatico extends Constantes
         int max = lexemas_tokens_correspondidos.size();
         posToken = 0;
         Controle c = analisar(TipoAnalise.a_inicioPrograma);
-        if(c!= null)
+        if (c != null)
             erros.add(Erro.getError(Erro.Inicio_da_Linguagem, c.getLexema()));
-        Token t;
+        c = analisar(TipoAnalise.a_chaves);
         boolean flag;
         while (posToken < max && !pilha_entrada.isEmpty())
         {
@@ -235,7 +234,7 @@ public class Sintatico extends Constantes
                 c = al_atribuicao();
                 break;
             case TipoAnalise.a_chaves:
-
+                c = al_chaves();
                 break;
             case TipoAnalise.ca_for:
                 c = al_for();
@@ -244,10 +243,15 @@ public class Sintatico extends Constantes
                 c = al_while();
                 break;
             case TipoAnalise.a_parenteses:
-
+                c = al_parenteses();
                 break;
             case TipoAnalise.ca_naoReconhecido:
                 c = al_Token_naoReconhecido();
+                break;
+            case TipoAnalise.ca_If:
+                c = al_if();
+                break;
+            case TipoAnalise.ca_else:
                 break;
         }
         return c;
@@ -263,26 +267,24 @@ public class Sintatico extends Constantes
         if (pilha_entrada.peek().getToken().equals(Token.tInicio_Linguagem))
         {
             pilha_entrada.pop();
-            if(pilha_entrada.peek().getToken().equals(Token.tIdentificador))
+            if (pilha_entrada.peek().getToken().equals(Token.tIdentificador))
             {
                 pilha_entrada.pop();
-                if(pilha_entrada.peek().getToken().equals(Token.tChave_abre))
+                if (pilha_entrada.peek().getToken().equals(Token.tChave_abre))
                 {
                     //retira chave da pilha
                     pilha_entrada.pop();
                     return null;
-                }
-                else
+                } else
                 {
-                    
+
                 }
-            }
-            else
+            } else
             {
-                
+
             }
         }
-        while(!pilha_entrada.isEmpty() && !pilha_entrada.pop().getToken().equals(Token.tChave_abre))
+        while (!pilha_entrada.isEmpty() && !pilha_entrada.pop().getToken().equals(Token.tChave_abre))
         {
         }
         return Erro.getError(Erro.Inicio_da_Linguagem, pilha_entrada.peek().getLexema());
@@ -409,14 +411,6 @@ public class Sintatico extends Constantes
             return null;
     }
 
-    private Controle regraNaoCompletada()
-    {
-        Erro error = Erro.getError(Erro.naoCompletado, pilha_entrada.pop().getLexema());
-        buscaTokenDeConexao();
-        posToken++;
-        return error;
-    }
-
     private Controle al_for()
     {
         Controle retorno = null;
@@ -444,17 +438,16 @@ public class Sintatico extends Constantes
                 {
                     pilha_entrada.pop();
                 }
-                
+
                 if (!pilha_entrada.isEmpty())
                 {
                     //parteDaExpressao
                     retorno = al_expressao();
-                }
-                else
+                } else
                 {
                     return regraNaoCompletada();
                 }
-                    
+
                 if (retorno instanceof Erro)
                 {
                     return retorno;
@@ -463,14 +456,13 @@ public class Sintatico extends Constantes
                 {
                     //parte da atribuicao
                     retorno = al_atribuicao2();
-                }
-                else
+                } else
                 {
                     return regraNaoCompletada();
                 }
             }
         }
-        if(pilha_entrada.peek().getToken().equals(Token.tParenteses_fecha))
+        if (pilha_entrada.peek().getToken().equals(Token.tParenteses_fecha))
             pilha_entrada.pop();
         return null;
     }
@@ -573,64 +565,175 @@ public class Sintatico extends Constantes
             return null;
     }
 
-    private Controle al_while() 
+    private Controle al_while()
     {
         Controle retorno = null;
-        
+
         Match m = pilha_entrada.pop();
         if (m.getToken().equals(Token.tWhile))
         {
-            if(pilha_entrada.peek().getToken().equals(Token.tParenteses_abre))
+            if (pilha_entrada.peek().getToken().equals(Token.tParenteses_abre))
             {
                 m = pilha_entrada.pop();
-                if(retorno == null && pilha_entrada.peek().getToken().equals(Token.tParenteses_fecha))
+                if (retorno == null && pilha_entrada.peek().getToken().equals(Token.tParenteses_fecha))
                 {
                     return null;
-                }
-                else
+                } else
                 {
                     retorno = al_expressao_boolean();
                 }
-            }
-            else
+            } else
                 retorno = Erro.getError(Erro.expressaoIlegal, pilha_entrada.peek().getLexema());
-        }
-        else
+        } else
             retorno = Erro.getError(Erro.naoCompletado, pilha_entrada.peek().getLexema());
-        
+        if (retorno == null && pilha_entrada.peek().getToken().equals(Token.tParenteses_fecha))
+        {
+            //retira o fecha parenteses
+            pilha_entrada.pop();
+        }
         return retorno;
     }
 
-    private Controle al_expressao_boolean() 
+    private Controle al_expressao_boolean()
     {
         Controle c = null;
         Match aux = pilha_entrada.pop();
-        if(Token.tValor_Bool.equals(aux.getToken()) && pilha_entrada.peek().getToken().equals(Token.tParenteses_fecha))
+        if (Token.tValor_Bool.equals(aux.getToken()) && pilha_entrada.peek().getToken().equals(Token.tParenteses_fecha))
         {
             return null;
-        }
-        else
+        } else
         {
-            while (!pilha_entrada.isEmpty() && 
-                    !pilha_entrada.peek().getToken().equals(Token.tParenteses_fecha) && 
-                    pilha_entrada.size()-3 >= 0)
-            {      
+            while (!pilha_entrada.isEmpty()
+                    && !pilha_entrada.peek().getToken().equals(Token.tParenteses_fecha)
+                    && pilha_entrada.size() - 3 >= 0)
+            {
                 //se valor(true ou false) e (operadorLogico) e valor(true ou false)
-                if(!((aux.getToken().equals(Token.tValor_Bool) || 
-                        aux.getToken().equals(Token.tIdentificador)) && 
-                        
-                        (Token.tOpLogicos.contains((aux = pilha_entrada.pop()).getToken()) ||
-                        Token.tOpRelacional.contains(aux.getToken())) &&
-                        
-                        (pilha_entrada.peek().getToken().equals(Token.tValor_Bool) || 
-                        pilha_entrada.peek().getToken().equals(Token.tIdentificador))))
+                if (!((aux.getToken().equals(Token.tValor_Bool)
+                        || aux.getToken().equals(Token.tIdentificador))
+                        && (Token.tOpLogicos.contains((aux = pilha_entrada.pop()).getToken())
+                        || Token.tOpRelacional.contains(aux.getToken()))
+                        && (pilha_entrada.peek().getToken().equals(Token.tValor_Bool)
+                        || pilha_entrada.peek().getToken().equals(Token.tIdentificador))))
                 {
                     c = Erro.getError(Erro.expressaoIlegal, pilha_entrada.peek().getLexema());
                     break;
-                }
+                } else
+                    aux = pilha_entrada.pop();
+
                 //aux = pilha_entrada.pop();
             }
         }
         return c;
+    }
+
+    private Controle al_if()
+    {
+        Controle res = null;
+        Match aux = pilha_entrada.pop();
+        if(aux.getToken().equals(Token.tIf) && pilha_entrada.pop().getToken().equals(Token.tParenteses_abre))
+        {
+            res = al_expressao_boolean();
+            if(res == null && pilha_entrada.peek().getToken().equals(Token.tParenteses_fecha))
+            {
+                //tira ) da pilha
+                pilha_entrada.pop();
+            }
+        }
+        else
+            return regraNaoCompletada(aux);
+        
+        return res;
+    }
+
+    private Controle regraNaoCompletada(Match aux)
+    {
+        Erro error = Erro.getError(Erro.naoCompletado, aux.getLexema());
+        buscaTokenDeConexao();
+        posToken++;
+        return error;
+    }
+    
+     private Controle regraNaoCompletada()
+    {
+        Erro error = Erro.getError(Erro.naoCompletado, pilha_entrada.pop().getLexema());
+        buscaTokenDeConexao();
+        posToken++;
+        return error;
+    }
+
+    private Controle al_chaves()
+    {
+        Controle res = null;
+        Stack<Match> abre = new Stack<>();
+        Stack<Match> fecha = new Stack<>();
+        for (Match l : lexemas_tokens_correspondidos)
+        {
+            if(l.getToken().equals(Token.tChave_abre))
+                abre.push(l);
+            else if(l.getToken().equals(Token.tChave_fecha))
+                fecha.push(l);
+        }
+        if(abre.size() == fecha.size())
+            return null;
+        for (int i = 0; i < abre.size() && !fecha.isEmpty(); i++)
+        {
+            abre.pop();
+            fecha.pop();
+        }
+        
+        if(fecha.isEmpty())
+            res = Erro.getError(Erro.chavesFaltantes, abre.peek().getLexema());
+        else
+            res = Erro.getError(Erro.chavesFaltantes, fecha.peek().getLexema());
+        
+        
+        for (Match cf : fecha)
+        {
+            erros.add(Erro.getError(Erro.chavesFaltantes, cf.getLexema()));
+        }
+        for (Match ca : abre)
+        {
+            erros.add(Erro.getError(Erro.chavesFaltantes, ca.getLexema()));
+        }
+        
+        return res;
+    }
+
+    private Controle al_parenteses()
+    {
+        Controle res = null;
+        Stack<Match> abre = new Stack<>();
+        Stack<Match> fecha = new Stack<>();
+        for (Match l : lexemas_tokens_correspondidos)
+        {
+            if(l.getToken().equals(Token.tParenteses_abre))
+                abre.push(l);
+            else if(l.getToken().equals(Token.tParenteses_fecha))
+                fecha.push(l);
+        }
+        if(abre.size() == fecha.size())
+            return null;
+        for (int i = 0; i < abre.size() && !fecha.isEmpty(); i++)
+        {
+            abre.pop();
+            fecha.pop();
+        }
+        
+        if(fecha.isEmpty())
+            res = Erro.getError(Erro.parenteseFaltante, abre.peek().getLexema());
+        else
+            res = Erro.getError(Erro.parenteseFaltante, fecha.peek().getLexema());
+        
+        
+        for (Match cf : fecha)
+        {
+            erros.add(Erro.getError(Erro.parenteseFaltante, cf.getLexema()));
+        }
+        for (Match ca : abre)
+        {
+            erros.add(Erro.getError(Erro.parenteseFaltante, ca.getLexema()));
+        }
+        
+        return res;
     }
 }
