cbfc4cdd2315c013b69cf6fc3e8f4e6460d5435e
Luís Henrique Picinin Jandre <luishpj@outlook.com.br>
2020-05-18 19:49:44 -0300

    if e while agr estão quase certos

--- src/Classes/Controle/Conversor.java
+++ src/Classes/Controle/Conversor.java
@@ -8,6 +8,7 @@ package Classes.Controle;
 import Classes.Lexema;
 import Classes.Token;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -18,24 +19,26 @@ import java.util.List;
  */
 public class Conversor
 {
-    
+
     public final static String declaracao = "toCI_declaracao";
     public final static String atribuicao = "toCI_atribuicao";
     public final static String For = "toCI_for";
     public final static String While = "toCI_while";
     public final static String Else = "toCI_else";
     public final static String If = "toCI_if";
-    
+
     public static List<InstrucaoIntermediaria> toCI_declaracao(Instrucao intr)
     {
         List<Match> listPrincipal = intr.getCadeia_elementos();
         List<InstrucaoIntermediaria> result = new ArrayList<>();
         List<Match> lInstr = new ArrayList<>();
+        retiraPontoVirgula(listPrincipal);
+
         lInstr.add(listPrincipal.get(0));//tipo
         lInstr.add(listPrincipal.get(1));//variavel
-        
+
         result.add(new InstrucaoIntermediaria(lInstr, declaracao, intr.getEscopo()));
-        
+
         if (listPrincipal.size() > 2)//possui atribuicao
         {
             List<Match> tmpLCE = new ArrayList<>(intr.getCadeia_elementos());
@@ -46,14 +49,15 @@ public class Conversor
         }
         return result;
     }
-    
+
     public static List<InstrucaoIntermediaria> toCI_atribuicao(Instrucao intr)
     {
-        
+
         Token tipoOriginal = intr.getCadeia_elementos().get(0).getToken();
+        Match variavel = intr.getCadeia_elementos().get(0);
         List<Match> listPrincipal = new ArrayList<>(intr.getCadeia_elementos());
-        if(listPrincipal.get(0).getToken().equals(Token.tIdentificador) && 
-                listPrincipal.get(1).getToken().equals(Token.tIgual))
+        if (listPrincipal.get(0).getToken().equals(Token.tIdentificador)
+                && listPrincipal.get(1).getToken().equals(Token.tIgual))
         {
             listPrincipal.remove(0);
             listPrincipal.remove(0);
@@ -69,20 +73,24 @@ public class Conversor
         map.put('•', ">=");
         map.put('○', "<=");
         map.put('♦', "!=");
-        map.put('=', "==");
+        map.put('♥', "==");
         Iterator iterator = map.keySet().iterator();
-        while(iterator.hasNext())
+        while (iterator.hasNext())
         {
-            char next = (char)iterator.next();
+            char next = (char) iterator.next();
             map_oper.put(map.get(next), next);
         }
-        
+
         List<Match> auxlist = new ArrayList<>();
-        
-        if (listPrincipal.size() == 3)//atribuicao simples
+
+        if (listPrincipal.size() <= 3)//atribuicao simples
         {
+            listPrincipal.add(0, new Match(new Lexema("=", 0, 0), Token.tIgual));
+            listPrincipal.add(0, variavel);
+
             result.add(new InstrucaoIntermediaria(new ArrayList<>(listPrincipal), atribuicao, intr.getEscopo()));
-        } else
+        }
+        else
         {
             //mapeamento chave valor
             listPrincipal.forEach(in ->
@@ -98,7 +106,8 @@ public class Conversor
                         {
                             aux = (char) (65 + map.size());
                             map.put(aux, in.getLexema().getPalavra());
-                        } else
+                        }
+                        else
                         {
                             char key = 0;
                             for (Object itemKey : map.keySet())
@@ -109,17 +118,19 @@ public class Conversor
                                     break;
                                 }
                             }
-                            aux = (char) map.get(key);
+                            //aux = (char) map.get(key);
+                            aux = key;
                         }
                         maux.getLexema().setPalavra(aux + "");
                     }
-                    else if(Token.tOpRelacional.contains(in.getToken()) || Token.tOpLogicos.contains(in.getToken()))//operador lógico
+                    else if (Token.tOpRelacional.contains(in.getToken()) || Token.tOpLogicos.contains(in.getToken()))//operador lógico
                     {
                         maux.getLexema().setPalavra(map_oper.get(in.getLexema().getPalavra()).toString());
                     }
                     auxlist.add(maux);
-                    
-                } catch (CloneNotSupportedException ex)
+
+                }
+                catch (CloneNotSupportedException ex)
                 {
                     System.out.println(ex.getMessage());
                 }
@@ -132,12 +143,12 @@ public class Conversor
                 exp_aux.append(in.getLexema().getPalavra());
             });
             String exp = exp_aux.toString().replaceAll("\\)|\\(", "");
-            
+
             ArvoreTresEnderecos arv = new ArvoreTresEnderecos();
             String aux;
             List<String> codigo_3_Enderecos = arv.build(exp);
             List<Match> ls_match_aux;
-            
+
             for (String codigoIns : codigo_3_Enderecos)
             {
                 ls_match_aux = new ArrayList<>();
@@ -150,9 +161,10 @@ public class Conversor
                             aux = (String) map.get(key.charAt(0));
                         else
                             aux = key;
-                        
+
                         m = getMatchPalavra(intr, aux);
-                    } else//variavel temporaria criada pelo codigo de 3
+                    }
+                    else//variavel temporaria criada pelo codigo de 3
                     {
                         m = new Match(new Lexema(key, 0, 0), tipoOriginal);
                     }
@@ -161,24 +173,45 @@ public class Conversor
                 result.add(new InstrucaoIntermediaria(ls_match_aux, atribuicao, intr.getEscopo()));
             }
             //faz a ultimalinha ser a variavel do codigo recebendo a ultima operacao
-            result.get(result.size() - 1).getCadeia_elementos().set(0, listPrincipal.get(0));
+            result.get(result.size() - 1).getCadeia_elementos().set(0, variavel);
         }
-        
+
         return result;
     }
-    
+
     public static List<InstrucaoIntermediaria> toCI_for(Instrucao intr)
     {
         return null;
     }
-    
+
     public static List<InstrucaoIntermediaria> toCI_while(Instrucao intr)
     {
-        return null;
+        List<InstrucaoIntermediaria> result = new ArrayList<>();
+        List<Match> lprincipal = new ArrayList<>(intr.getCadeia_elementos());
+        List<Match> laux = new ArrayList<>();
+        lprincipal.remove(0);//tira o while
+        if (lprincipal.get(0).getToken().equals(Token.tParenteses_abre))
+            lprincipal.remove(0);//tira o (
+        
+        lprincipal.add(0, new Match(new Lexema("=", 0, 0), Token.tIgual));
+        lprincipal.add(0, new Match(new Lexema("vWhile", 0, 0), Token.tIdentificador));
+        Instrucao int_aux = new Instrucao(atribuicao);
+        int_aux.setCadeia_elementos(lprincipal);
+        
+        result.addAll(toCI_atribuicao(int_aux));
+        laux.add(new Match(new Lexema("loop", 0, 0), Token.tWhile));
+        laux.add(new Match(new Lexema("vWhile", 0, 0), Token.tIdentificador));
+        laux.add(new Match(new Lexema("goto" + 0, 0, 0), Token.tgoto));
+        
+        InstrucaoIntermediaria ii = new InstrucaoIntermediaria(laux, While, intr.getEscopo());
+        
+        result.add(ii);
+        return result;
     }
-    
+
     public static List<InstrucaoIntermediaria> toCI_if(Instrucao intr)
     {
+        
         List<InstrucaoIntermediaria> result = new ArrayList<>();
         List<Match> lprincipal = new ArrayList<>(intr.getCadeia_elementos());
         List<Match> laux = new ArrayList<>();
@@ -200,34 +233,36 @@ public class Conversor
         
         result.add(ii);
         return result;
+        
     }
-    
+
     public static List<InstrucaoIntermediaria> toCI_else(Instrucao intr)
     {
         return null;
     }
-    
+
     private static Match getMatchPalavra(Instrucao intr, String palavra)
     {
         List<Match> ce = intr.getCadeia_elementos();
         int i;
         for (i = 0; i < ce.size() && !ce.get(i).getLexema().getPalavra().equals(palavra); i++)
         {
-            
+
         }
         return i < ce.size() ? ce.get(i) : null;
     }
-    
+
     private static void retiraPontoVirgula(List<Match> listPrincipal)
     {
         List<Match> mremocao = new ArrayList<>();
+        List<Token> remover = Arrays.asList(Token.tPontoVirgula, Token.tParenteses_abre, Token.tParenteses_fecha);
         
         listPrincipal.forEach(in ->
         {
-            if (in.getToken().equals(Token.tPontoVirgula))
+            if (remover.contains(in.getToken()))
                 mremocao.add(in);
         });
         listPrincipal.removeAll(mremocao);
     }
-    
+
 }
